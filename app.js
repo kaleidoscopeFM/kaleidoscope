2dd901518a55a582c0505d6a9280b46b9f2318dd
thanhbrandon <thanhbrandon@tamu.edu>
2023-03-05 07:04:15 -0600

    adds review button

--- app.js
+++ app.js
@@ -15,7 +15,9 @@ const TOKEN = "https://accounts.spotify.com/api/token";
 const PLAYER = "https://api.spotify.com/v1/me/player";
 const TRACKS = "https://api.spotify.com/v1/playlists/{{PlaylistId}}/tracks";
 const CURRENTLYPLAYING = "https://api.spotify.com/v1/me/player/currently-playing";
-
+const openModalButtons = document.querySelectorAll('[data-modal-target]');
+const closeModalButtons = document.querySelectorAll('[data-close-button]');
+const overlay = document.getElementById('overlay')
 function LinkedList() {
     var length = 0;
     var head = null;
@@ -383,3 +385,35 @@ function handleCurrentlyPlayingResponse(){
     }
 }
 
+openModalButtons.forEach(button => {
+    button.addEventListener('click', () => {
+      const modal = document.querySelector(button.dataset.modalTarget)
+      openModal(modal)
+    })
+  })
+  
+overlay.addEventListener('click', () => {
+const modals = document.querySelectorAll('.modal.active')
+modals.forEach(modal => {
+    closeModal(modal)
+})
+})
+
+closeModalButtons.forEach(button => {
+button.addEventListener('click', () => {
+    const modal = button.closest('.modal')
+    closeModal(modal)
+})
+})
+
+function openModal(modal) {
+if (modal == null) return
+modal.classList.add('active')
+overlay.classList.add('active')
+}
+
+function closeModal(modal) {
+if (modal == null) return
+modal.classList.remove('active')
+overlay.classList.remove('active')
+}
