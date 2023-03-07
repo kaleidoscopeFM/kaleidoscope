94173b9ead24891bdaf5266b6f3456ecf9bd5b95
thanhbrandon <thanh.brandon.tran@gmail.com>
2023-03-07 00:13:23 -0600

    Adds one working modal button and updates the bootstrap

--- app.js
+++ app.js
@@ -385,35 +385,4 @@ function handleCurrentlyPlayingResponse(){
     }
 }
 
-openModalButtons.forEach(button => {
-    button.addEventListener('click', () => {
-      const modal = document.querySelector(button.dataset.modalTarget)
-      openModal(modal)
-    })
-  })
-  
-overlay.addEventListener('click', () => {
-const modals = document.querySelectorAll('.modal.active')
-modals.forEach(modal => {
-    closeModal(modal)
-})
-})
 
-closeModalButtons.forEach(button => {
-button.addEventListener('click', () => {
-    const modal = button.closest('.modal')
-    closeModal(modal)
-})
-})
-
-function openModal(modal) {
-if (modal == null) return
-modal.classList.add('active')
-overlay.classList.add('active')
-}
-
-function closeModal(modal) {
-if (modal == null) return
-modal.classList.remove('active')
-overlay.classList.remove('active')
-}
